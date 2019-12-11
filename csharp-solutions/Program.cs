7ece7e3befdc7ae6fac6fd33fb25ddf9f6cfc6af
omorka <onyeka321@yahoo.com>
2019-12-10 20:40:56 -0500

    work on day 1

--- csharp-solutions/Program.cs
+++ csharp-solutions/Program.cs
@@ -8,7 +8,49 @@ class Program
 {
 	static void Main(string[] args)
 	{
+        List<int> numbers = new List<int>();
+
 		Console.WriteLine("Hello world!");
-		Console.ReadLine();
-	}
+        string input = Console.ReadLine();
+
+        try
+        {
+            foreach (char a in input.ToCharArray())
+            {
+                numbers.Add((int)Char.GetNumericValue(a));
+            }
+            foreach (int s in numbers)
+            {
+                Console.WriteLine(s.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        int newSum = SumOfDigits(numbers);
+        Console.WriteLine(newSum.ToString());
+    }
+
+    static int SumOfDigits(List<int> digits)
+    {
+        int sum = 0;
+        for (int i=0; i < digits.Count; i++)
+        {
+            if (i == digits.Count-1)
+            {
+                if (digits[i] == digits.First())
+                {
+                    sum += digits[i];
+                }
+            }
+
+            else if (digits[i] == digits[i+1])
+            {
+                sum += digits[i];
+            }
+        }
+        return sum;
+    }
 }
