5af29b14d7121d520aa06a89054c64179cea21a3
James Keats <jkeats@blizzard.com>
2019-12-11 11:37:10 -0800

    Add our C++ solution for day 1

--- Day1/cpp-solutions/main.cpp
+++ Day1/cpp-solutions/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+int SumOfDigits(std::vector<int>& digits);
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    std::vector<int> numbers;
+
+    std::ifstream fileIn("input.txt");
+    std::string input;
+    std::getline(fileIn, input);
+
+    for (char a : input)
+    {
+        numbers.push_back(a - '0');
+    }
+
+    std::cout << SumOfDigits(numbers);
+
     std::cin.get();
     return 0;
+}
+
+int SumOfDigits(std::vector<int>& digits)
+{
+    int sum = 0;
+    for (int i = 0; i < digits.size(); i++)
+    {
+        if (i == digits.size() - 1)
+        {
+            if (digits[i] == digits.front())
+            {
+                sum += digits[i];
+            }
+        }
+
+        else if (digits[i] == digits[i + 1])
+        {
+            sum += digits[i];
+        }
+    }
+    return sum;
 }
